22a48474392def187707b6dfdf754c1190626ab8
Ilya Kholodilov <kholodilov0601@gmail.com>
2025-04-21 18:37:28 +0300

    Avtotest from "automation-practice-form"

---
+++ src/test/java/SimpleJunitTest.java
@@ -0,0 +1,60 @@
+package src.test.java;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.SPACE;
+
+public class SimpleJunitTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.pageLoadStrategy ="eager";
+    }
+
+    @AfterEach
+    void closeWebDriver(){
+        Selenide.closeWebDriver();
+    }
+
+    @Test
+    void fillFormTest() {
+        open("automation-practice-form");
+        $("#firstName").setValue("Илья");
+        $("#lastName").setValue("Холодилов");
+        $("#userEmail").setValue("holod06@yandex.ru");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("9958332203");
+        $("[id=dateOfBirthInput]").sendKeys(Keys.CONTROL + "A");
+        $("[id=dateOfBirthInput]").sendKeys(SPACE);
+        $("#dateOfBirthInput").setValue("6 January 1999").pressEnter();
+        $("#subjectsInput").setValue("Arts").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("retouch.jpg");
+        $("#currentAddress").setValue("Moscow, м. Потапово").pressEnter();
+        $("#react-select-3-input").setValue("Uttar Pradesh").pressEnter();
+        $("#react-select-4-input").setValue("Agra").pressEnter();
+        $("#submit").click();
+
+
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Илья Холодилов"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("holod06@yandex.ru"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9958332203"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("06 January,1999"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Arts"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("retouch.jpg"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Moscow, м. Потапово"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("Uttar Pradesh Agra"));
+    }
+}
